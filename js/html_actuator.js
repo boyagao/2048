6144694ec7558fa9b06ca418cf0cafda845bb6f9
Boya Gao <boyagao@gmail.com>
2021-08-13 19:05:35 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -2,8 +2,7 @@ function HTMLActuator() {
   this.tileContainer    = document.querySelector(".tile-container");
   this.scoreContainer   = document.querySelector(".score-container");
   this.bestContainer    = document.querySelector(".best-container");
-  this.messageContainer = document.querySelector(".game-message");
-
+  
   this.score = 0;
 }
 
@@ -130,8 +129,8 @@ HTMLActuator.prototype.message = function (won) {
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
 };
 
-HTMLActuator.prototype.clearMessage = function () {
-  // IE only takes one value to remove at a time.
-  this.messageContainer.classList.remove("game-won");
-  this.messageContainer.classList.remove("game-over");
-};
+// HTMLActuator.prototype.clearMessage = function () {
+//   // IE only takes one value to remove at a time.
+//   this.messageContainer.classList.remove("game-won");
+//   this.messageContainer.classList.remove("game-over");
+// };
