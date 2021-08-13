a6728a73c8856cbf0786982fb29855831e5f4b20
Boya Gao <boyagao@gmail.com>
2021-08-13 19:16:25 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -2,6 +2,7 @@ function HTMLActuator() {
   this.tileContainer    = document.querySelector(".tile-container");
   this.scoreContainer   = document.querySelector(".score-container");
   this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
   
   this.score = 0;
 }
@@ -26,6 +27,8 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
     if (metadata.terminated) {
       if (metadata.over) {
         self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
       }
     }
 
